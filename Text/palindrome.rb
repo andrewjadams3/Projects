c1b08a79b23821f5538eef40d8c5f66973a9b06b
Andrew Adams <andrewjadams3@gmail.com>
2014-03-21 14:05:37 -0500

    Better palindrome solution

--- Text/palindrome.rb
+++ Text/palindrome.rb
@@ -1,10 +1,10 @@
 def simple_palindrome(string)
-	string = string.split(' ').join.downcase!
-	string == string.reverse
+	letters = string.split(/\W+/).join.downcase
+	letters == letters.reverse
 end
 
 #Test cases
-puts simple_palindrome("A but tuba") == true
+puts simple_palindrome("A but tuba.") == true
 puts simple_palindrome("Is this a palindrome?") == false
-
-#Does not work with punctuation. Future update?
+puts simple_palindrome("A man, a plan, a canal: panama.") == true
+puts simple_palindrome("\"Rum… rum…\" I murmur.") == true
